8c11c265b6f79debebccab2ebcd583ea5bed8a8b
Joseceron101 <2430933@clases.edu.sv>
2024-03-21 22:01:18 -0600

    diseño y ajustes modelo

--- HYCMJACRFJMA/Models/Users.cs
+++ HYCMJACRFJMA/Models/Users.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HYCMJACRFJMA.Models
 {
-    public class Users
+    public class  Users
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
