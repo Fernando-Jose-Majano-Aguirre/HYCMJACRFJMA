1505c39184d7d200909d31341fa88a61ce90b203
Fernando Jose Majano Aguirre <fernando13522477@outlook.com>
2024-03-21 21:55:11 -0600

    Modelos de la tabla y contexto creadas

---
+++ HYCMJACRFJMA/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace HYCMJACRFJMA.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Users> Usuarios { get; set; }
+    }
+}
--- HYCMJACRFJMA/Models/Users.cs
+++ HYCMJACRFJMA/Models/Users.cs
@@ -1,7 +1,39 @@
-﻿namespace HYCMJACRFJMA.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HYCMJACRFJMA.Models
 {
     public class Users
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = "El nombre de usuario es requerido.")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El apellido de usuario es requerido.")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El email de usuario es requerido.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 100 caracteres.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "El estatus  es requerido.")]
+        public byte Estatus { get; set; }
+        [Required(ErrorMessage = "El Rol de usuario es requerido.")]
+        public string Rol { get; set; }
+
+        [NotMapped] // Esta propiedad no será mapeada a la base de datos
+        [Compare("Password", ErrorMessage = "La confirmación de contraseña no coincide.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 100 caracteres.")]
+        [DataType(DataType.Password)]
+        public string ConfirmarPassword { get; set; }
+        [NotMapped]
+        public int Take { get; set; }
     }
 }
